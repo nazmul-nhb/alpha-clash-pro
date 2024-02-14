b6b5f2337bb8696f0838517373f8b13a306d9405
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-02-14 22:59:37 +0600

    added score functional score and life update

--- scripts/clash-script.js
+++ scripts/clash-script.js
@@ -6,6 +6,43 @@ function play(){
     playGround.classList.remove('hidden');
 }
  */
+function keyboardButtonPress(keyboard) {
+    const playerPressed = keyboard.key;
+    // console.log('player pressed', playerPressed);
+
+    // what key the player is expected to press
+    const currentAlphabetElement = document.getElementById('current-letter');
+    const currentLetter = currentAlphabetElement.innerText;
+    const expectedLetter = currentLetter.toLowerCase();
+    console.log(playerPressed, expectedLetter);
+
+    // check if matched
+    if (playerPressed === expectedLetter) {
+        // console.log('you got a point');
+        // update score
+        const currentScoreElement = document.getElementById('score-update');
+        const currentScoreString = document.getElementById('score-update').innerText;
+        const currentScore = parseInt(currentScoreString);
+        const updatedScore = currentScore + 1;
+        // display score
+        currentScoreElement.innerText = updatedScore;
+        // start a new round
+        removeBgColorById(playerPressed);
+        continueGame();
+    }
+    else {
+        // console.log('you missed');
+        const currentLifeElement = document.getElementById('life-update');
+        const currentLifeString = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeString);
+        const updatedLife = currentLife - 1;
+        // display life
+        currentLifeElement.innerText = updatedLife;
+    }
+}
+// captured keyboard key press
+document.addEventListener('keyup', keyboardButtonPress) // callback function
+
 
 function continueGame() {
     // 1. generate a random alphabet
--- scripts/utility.js
+++ scripts/utility.js
@@ -15,6 +15,11 @@ function setBgColorById(id) {
     element.classList.add('bg-orange-400');
 }
 
+function removeBgColorById(id){
+    const element = document.getElementById(id);
+    element.classList.remove('bg-orange-400');
+}
+
 function getARandomLetter() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split(''); //creating an alphabet array
